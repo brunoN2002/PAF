6543a30d407c740a30094a213d96748a7bb6c8fb
SpaceXAlphaBravoCharlie <mjankules@pmfst.hr>
2022-03-04 12:25:03 +0100

    vjezbe

---
+++ vjezbe/vjezbe1/zadatak2.py
@@ -0,0 +1,5 @@
+y1=int(input("x kordinate prve tocke: "))
+x1=int(input("y kordinate prve tocke: "))
+y2=int(input("x kordinate druge tocke: "))
+x2=int(input("y kordinate druge tocke: "))
+y=((y2-y1)/(x2-x1))*("x"-x1)-y1
