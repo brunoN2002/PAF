8be287c163cc01741ece53d2b36a5de0de2e082c
kvilibic <kvilibic@pmfst.hr>
2022-03-11 12:47:52 +0100

    vjezbe na satu, 11.3.2022

--- vjezbe/vjezbe1/zadatak6.py
+++ vjezbe/vjezbe1/zadatak6.py
@@ -1,6 +1,12 @@
-def tocka_i_kruznica():
-    r=float(input("Upišite radijus kružnice: "))
-    p=float(input("Upišite x kordinate ishodišta kružnice: "))
-    q=float(input("Upišite y kordinate ishodišta kružnice: "))
-    x=float(input("Upišite x kordinatu tocke: "))
-    y=float(input())
+import math as math
+#r je radijus funkcije, p je x kor. središta, q je y kor. središta, x i y su kordinate tocke.
+def tocka_i_kruznica(r,p,q,x,y):
+    r2=math.sqrt((x-p)**2+(y-q)**2)
+    if r2>r:
+        print("tocka se nalazi izvan kružnice")
+    if r2<r:
+        print("tocka se nalazi unutar kružnice")
+    if r2==r:
+        print("tocka se nalazi na kruznici")
+
+tocka_i_kruznica(5,0,0,1,1)
--- vjezbe/vjezbe2/zadatak1.py
+++ vjezbe/vjezbe2/zadatak1.py
@@ -1,12 +1,37 @@
 import numpy as np
-import matplotlib as plt
-def jednoliko_gibanje(F, m, v0):
+import matplotlib.pyplot as plt
+def jednoliko_gibanje(F, m):
     a=F/m
     brzina=list()
-    v=v0
-    for dt in np.arange(0, 10, 0.1):
-        v=v+a*dt
+    vrijeme=list()
+    pomak=list()
+    akceleracija=list()
+    dt=0.1
+    v0=0
+    t0=0
+    x0=0
+    for i in np.arange(0, 10, dt):
+        v=v0+a*dt
+        v0=v
+        t0+=dt
+        x=x0+v*dt
+        x0=x
+        pomak.append(x)
+        vrijeme.append(t0)
         brzina.append(v)
-    print(brzina)
+        akceleracija.append(a)
+    plt.plot(vrijeme, brzina)
+    plt.xlabel("vrijeme [s]")
+    plt.ylabel("brzina [m/s]")
+    plt.show()
+    plt.plot(vrijeme, pomak)
+    plt.xlabel("vrijeme [s]")
+    plt.ylabel("pomak [m]")
+    plt.show()
+    plt.plot(vrijeme, akceleracija)
+    plt.xlabel("vrijeme [s]")
+    plt.ylabel("akceleracija [m/s^2]")
+    plt.show()
 
-jednoliko_gibanje(9.81, 1, 0)
+
+jednoliko_gibanje(9.81, 1)
