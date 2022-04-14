3002eeb6231574f9ac745df2332a76d88a795768
KlaraLazic <klazic@pmfst.hr>
2022-04-14 09:12:04 +0200

    predavanja 14.4.2022

--- vjezbe/vjezbe6/zadatak3.cpp
+++ vjezbe/vjezbe6/zadatak3.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+#include <list>
+
+void cijeli_brojevi(float a, float b) {
+    for (int i=a; i<b; i++;) {
+        list<int> brojevi;
+        brojevi.assign(i);
+        std::cout << brojevi << std::endl;
+    }
+}
+
+int main {
+    cijeli_brojevi(2,7);
+    return 0;
+}
