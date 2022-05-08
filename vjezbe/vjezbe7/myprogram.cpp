a8999af824af3a03eab1491a5269e563e70bf689
Danijela LN <bnovakovi@pmfst.hr>
2022-05-08 20:28:08 +0200

    vjezbe 7, nedilja popodne 8.05.2022.

--- vjezbe/vjezbe7/myprogram.cpp
+++ vjezbe/vjezbe7/myprogram.cpp
@@ -3,7 +3,13 @@
 
 using namespace std;
 
-int main() {
-    Particle p1(100,45,0,0);
-    Particle p2(100,60,0,0);
+int main()
+{
+Particle p1(10,45,0,0);
+Particle p2(30,0.5,10,20);
+p1.range(0.01);
+p1.time(0.01);
+p2.range(0.01);
+p2.time(0.01);
+return(0);
 }
