ef7f1e10a319c0fdd5366f9a408f0e9810b3e0ea
KlaraLazic <klazic@pmfst.hr>
2022-05-20 12:36:40 +0200

    efier

--- vjezbe/vjezbe10/zadatak1_test.py
+++ vjezbe/vjezbe10/zadatak1_test.py
@@ -2,12 +2,12 @@ import zadatak1 as zd
 import matplotlib.pyplot as plt
 import numpy as np
 
-p1=zd.ElectricForce()
-p1.set_initial_conditions(np.array((0.1,0.1,0.1)),np.array((0,0,0)),np.array((0,0,0)),np.array((0,0,1)),-1,1,0.01)
-p1.plot_trajectory(10)
+electron=zd.ElectricForce()
+electron.set_initial_conditions(np.array((0.1,0.1,0.1)),np.array((0,0,0)),np.array((0,0,0)),np.array((0,0,1)),-1,1,0.01)
+electron.plot_trajectory(10)
 
-p2=zd.ElectricForce()
-p2.set_initial_conditions(np.array((0.1,0.1,0.1)),np.array((0,0,0)),np.array((0,0,0)),np.array((0,0,1)),1,1,0.01)
-p2.plot_trajectory(10)
+positron=zd.ElectricForce()
+positron.set_initial_conditions(np.array((0.1,0.1,0.1)),np.array((0,0,0)),np.array((0,0,0)),np.array((0,0,1)),1,1,0.01)
+positron.plot_trajectory(10)
 
 plt.show()
