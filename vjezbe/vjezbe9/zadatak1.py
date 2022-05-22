c7c1be030754b4ce9d6f36854bc7447830886711
Danijela LN <bnovakovi@pmfst.hr>
2022-05-22 23:12:52 +0200

    nedilja navecer 22.5.2022

--- vjezbe/vjezbe9/zadatak1.py
+++ vjezbe/vjezbe9/zadatak1.py
@@ -8,6 +8,10 @@ class BungeeJumping:
         self.a=list()
         self.v=list()
         self.x=list()
+        self.kineticka=list()
+        self.gravitacijska=list()
+        self.elasticna=list()
+        self.ukupna=list()
 
     def set_initial_conditions(self, h, l, k, c, m, dt):
         self.h=h
@@ -22,6 +26,10 @@ class BungeeJumping:
         self.v.append(0)
         self.y.append(h)
         self.x.append(0)
+        self.kineticka.append(self.m*(self.v[-1])**2/2)
+        self.gravitacijska.append(self.m*9.81*self.y[-1])
+        self.elasticna.append(self.k*(self.x[-1])**2/2)
+        self.ukupna.append(self.kineticka[-1]+self.gravitacijska[-1]+self.elasticna[-1])
 
     def __Fe(self, x):
         return self.k*x
@@ -30,30 +38,38 @@ class BungeeJumping:
         return -1*np.sign(v)*self.c*(v)**2
 
     def __move(self):
-        if (self.h-self.l-self.y[-1])<0:
-            self.x.append(0)
-        else:
+        if np.abs(self.y[-1]-self.h)>self.l:
             self.x.append(self.h-self.l-self.y[-1])
+        else:
+            self.x.append(0)
         self.vrijeme.append(self.vrijeme[-1]+self.dt)
         self.v.append(self.v[-1]+self.a[-1]*self.dt)
         self.a.append(self.g+(self.__Fe(self.x[-1])/self.m)+(self.__Fo(self.v[-1])/self.m))
         self.y.append(self.y[-1]+self.v[-1]*self.dt)
+        self.kineticka.append(self.m*(self.v[-1])**2/2)
+        self.gravitacijska.append(self.m*9.81*self.y[-1])
+        self.elasticna.append(self.k*(self.x[-1])**2/2)
+        self.ukupna.append(self.kineticka[-1]+self.gravitacijska[-1]+self.elasticna[-1])
+
 
     def graf(self, t):
-        for i in range(100000):
+        N=int(t/self.dt)
+        for i in range(N):
             self.__move()
-        plt.plot(self.vrijeme,self.y)
+        plt.plot(self.vrijeme,self.ukupna, c="red")
+        plt.plot(self.vrijeme,self.kineticka, c="blue")
+        plt.plot(self.vrijeme,self.gravitacijska, c="green")
+        plt.plot(self.vrijeme,self.elasticna, c="yellow")
         plt.show()
 
     def __ay2(self, x, v, t):
-        return self.g+(self.k*x)+(-1*np.sign(v)*self.c*(v)**2)
+        return self.g+((self.k*x)/self.m)+((-1*np.sign(v)*self.c*(v)**2)/self.m)
 
     def __moveRK(self):
-        if (self.h-self.l-self.y[-1])<0:
-            self.x.append(0)
-        else:
+        if np.abs(self.y[-1]-self.h)>self.l:
             self.x.append(self.h-self.l-self.y[-1])
-
+        else:
+            self.x.append(0)
         k1vy=self.__ay2(self.x[-1], self.v[-1], self.vrijeme[-1])*self.dt
         k1y=self.v[-1]*self.dt
         k2vy=self.__ay2(self.x[-1]+k1y/2, self.v[-1]+k1vy/2, self.vrijeme[-1]+self.dt/2)*self.dt
@@ -66,6 +82,11 @@ class BungeeJumping:
         self.vrijeme.append(self.vrijeme[-1]+self.dt)
         self.v.append(self.v[-1]+(1/6)*(k1vy+2*k2vy+2*k3vy+k4vy))
         self.y.append(self.y[-1]+(1/6)*(k1y+2*k2y+2*k3y+k4y))
+
+        self.kineticka.append(self.m*(self.v[-1])**2/2)
+        self.gravitacijska.append(self.m*9.81*self.y[-1])
+        self.elasticna.append(self.k*(self.x[-1])**2/2)
+        self.ukupna.append(self.kineticka[-1]+self.gravitacijska[-1]+self.elasticna[-1])
         
     def grafRK(self, t):
         N=int(t/self.dt)
@@ -73,5 +94,8 @@ class BungeeJumping:
             self.__moveRK()
         #print(self.y)
         #print(self.vrijeme)
-        plt.plot(self.vrijeme,self.y)
+        plt.plot(self.vrijeme,self.ukupna, c="red")
+        plt.plot(self.vrijeme,self.kineticka, c="blue")
+        plt.plot(self.vrijeme,self.gravitacijska, c="green")
+        plt.plot(self.vrijeme,self.elasticna, c="yellow")
         plt.show()
--- vjezbe/vjezbe9/zadatak1_test.py
+++ vjezbe/vjezbe9/zadatak1_test.py
@@ -1,9 +1,13 @@
 import zadatak1 as zd
 
-#p1=zd.BungeeJumping()
-#p1.set_initial_conditions(1000, 30, 10, 0, 80, 0.01)
-#p1.graf(100)
+p1=zd.BungeeJumping()
+p1.set_initial_conditions(100, 50, 100, 0, 80, 0.001)
+p1.grafRK(20)
 
 p2=zd.BungeeJumping()
-p2.set_initial_conditions(100, 30, 100, 0, 80, 0.0001)
-p2.grafRK(50)
+p2.set_initial_conditions(100, 50, 100, 0, 80, 0.001)
+p2.graf(20)
+
+p3=zd.BungeeJumping()
+p3.set_initial_conditions(100, 50, 100, 1.5, 80, 0.01)
+p3.grafRK(100)
